2817f2f0742ea10724acca8f937bb588b0742798
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-18 16:47:50 -0300

    creating global theme colors and fonts

---
+++ src/global/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+	colors: {
+		background: "#F4F5F6",
+		green: "#66953F",
+		orange: "#BA642A",
+		white: "#FFF",
+		black: "#000",
+	},
+
+	fonts: {
+		title700: "Nunito_700Bold",
+		title600: "Nunito_600SemiBold",
+		text400: "Play_400Regular",
+	},
+};
