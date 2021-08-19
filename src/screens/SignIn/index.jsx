609f69bee465c013f89b084c7c4cc8d03cdb0e27
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-19 11:47:01 -0300

    creating the ImageBackground

--- src/screens/SignIn/index.jsx
+++ src/screens/SignIn/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-import { View } from "react-native";
-
+import { Image, ImageBackground, Text, View } from "react-native";
+import SignInScreenImg from "../../assets/SignInScreen.png";
 import { styles } from "./styles";
 
 export function SignIn() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				source={SignInScreenImg}
+				style={styles.backgroundImage}
+				resizeMode="stretch"
+			>
+				<View style={styles.content}>
+					<Text style={styles.text}>TESTE TESTE TESTE</Text>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -3,5 +3,27 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	backgroundImage: {
+		width: "100%",
+		height: "100%",
+	},
+
+	content: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	text: {
+		fontSize: 20,
+		marginTop: 200,
 	},
 });
