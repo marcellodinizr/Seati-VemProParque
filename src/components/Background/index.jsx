69d58a6c4e6996d854a7a8bc16c78ccfeea0dc8d
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-20 11:04:08 -0300

    creating Background component

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+import { theme } from "../../global/styles/theme";
+import { styles } from "./styles";
+
+export function Background() {
+	return <View style={styles.container}></View>;
+}
