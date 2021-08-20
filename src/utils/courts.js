30a78a7d061a1b623f804c3341e42bc1fa6d32b9
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-20 11:03:13 -0300

    creating courts and parks utils

---
+++ src/utils/courts.js
@@ -0,0 +1,9 @@
+import SportsCourtSvg from "../assets/sports_court.svg";
+import SandCourtSvg from "../assets/sand_court.svg";
+import TennisCourtSvg from "../assets/tennis_court.svg";
+
+export const courts = [
+	{ id: "1", title: "Quadra Poliesportiva", icon: SportsCourtSvg },
+	{ id: "2", title: "Quadra de Areia", icon: SandCourtSvg },
+	{ id: "3", title: "Quadra de Tênis", icon: Tennis },
+];
---
+++ src/utils/parks.js
@@ -0,0 +1,8 @@
+import IconSvg from "../assets/icon_small.svg";
+
+export const parks = [
+	{ id: "1", title: "Parque da Lagoa", icon: IconSvg },
+	{ id: "2", title: "Parque do Itapiracó", icon: IconSvg },
+	{ id: "3", title: "Parque do Sucupira", icon: IconSvg },
+	{ id: "4", title: "Parque do Rangedor", icon: IconSvg },
+];
