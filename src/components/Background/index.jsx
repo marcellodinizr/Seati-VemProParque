94f455d73af952756fae26da4f32c5067b9eaa98
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-22 18:09:30 -0300

    changing the Dimensions and adding backgroundColor

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import { View } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 
-import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 
 export function Background({ children }) {
-	return (
-		<LinearGradient
-			style={styles.container}
-			colors={[theme.colors.background, theme.colors.green]}
-		>
-			{children}
-		</LinearGradient>
-	);
+	return <View style={styles.container}>{children}</View>;
 }
--- src/components/Background/styles.js
+++ src/components/Background/styles.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Dimensions } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
 	container: {
-		height: Dimensions.get("window").height,
+		height: Dimensions.get("screen").height,
+		backgroundColor: theme.colors.background,
 	},
 });
