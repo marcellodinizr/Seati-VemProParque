3f3a4a5535bfdcd025ec574bfff598356e90bffa
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-24 11:35:57 -0300

    renaming to AppointmentPark

--- src/screens/AppointmentPark/index.jsx
+++ src/screens/AppointmentPark/index.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 
+import { useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
+
 import { ParkSelect } from "../../components/ParkSelect";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 
 import { styles } from "./styles";
-import { ScrollView } from "react-native-gesture-handler";
 
-export function SelectPark() {
+export function AppointmentPark() {
+	const navigation = useNavigation();
+
 	const [park, setPark] = useState("");
+
+	function handleAppointmentCourt() {
+		navigation.navigate("AppointmentCourt");
+	}
 
 	return (
 		<Background>
 			<View style={styles.container}>
 				<Header title="Selecione o Parque" />
 				<ScrollView>
-					<ParkSelect setPark={setPark} parkSelected={park} />
+					<ParkSelect
+						setPark={setPark}
+						parkSelected={park}
+						onPress={handleAppointmentCourt}
+					/>
 				</ScrollView>
 			</View>
 		</Background>
