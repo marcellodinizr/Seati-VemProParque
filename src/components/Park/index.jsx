f3cfc9d7d6805cfdf4f86febe601568e2321e581
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-26 11:36:56 -0300

    adding checked when press on park

--- src/components/Park/index.jsx
+++ src/components/Park/index.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+
+import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 
-export function Park({ title, icon: Icon, ...rest }) {
+export function Park({
+	title,
+	icon: Icon,
+	checked = false,
+	hasCheckBox = false,
+	...rest
+}) {
 	return (
 		<RectButton {...rest}>
 			<View style={styles.container}>
-				<View style={styles.content}>
-					<View />
-
+				<View style={[styles.content, { opacity: checked ? 1 : 0.7 }]}>
 					<Icon width={52} height={48} />
 
-					<Text style={styles.title}>{title}</Text>
+					<Text
+						style={[
+							styles.title,
+							{ color: checked ? theme.colors.on : theme.colors.green },
+						]}
+					>
+						{title}
+					</Text>
 				</View>
 			</View>
 		</RectButton>
