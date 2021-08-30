8dc14b4d933325c252e8fd428d703acb17df99f8
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-30 11:40:22 -0300

    adding CalendarSchedule inside AppointmentSchedule

--- src/screens/AppointmentSchedule/index.jsx
+++ src/screens/AppointmentSchedule/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { View } from "react-native";
 import { Background } from "../../components/Background";
+import { CalendarSchedule } from "../../components/CalendarSchedule";
 import { Header } from "../../components/Header";
 
 import { styles } from "./styles";
@@ -11,6 +12,9 @@ export function AppointmentSchedule() {
 		<Background>
 			<View style={styles.container}>
 				<Header title="Agendamento" />
+				<View style={styles.content}>
+					<CalendarSchedule />
+				</View>
 			</View>
 		</Background>
 	);
