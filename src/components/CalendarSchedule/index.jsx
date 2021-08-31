17b8ded5d24133cc978e2c6f3e29fd1a10c9045d
marcellodinizr <marcellodinizrocha@gmail.com>
2021-08-31 11:34:41 -0300

    trying DateTimePicker for Schedulec

--- src/components/CalendarSchedule/index.jsx
+++ src/components/CalendarSchedule/index.jsx
@@ -1,19 +1,52 @@
-import React from "react";
-import { View } from "react-native";
-import { Calendar } from "react-native-calendario";
+import React, { useState } from "react";
+import { View, Button, Platform, Text } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 import { styles } from "./styles";
 
 export function CalendarSchedule() {
-	const [date, setDate] = useState(new Date());
+	const [date, setDate] = useState(new Date(1598051730000));
+	const [mode, setMode] = useState("date");
+	const [show, setShow] = useState(false);
 
-	const onChange = (date) => {
-		setDate(date);
+	const onChange = (event, selectedDate) => {
+		const currentDate = selectedDate || date;
+		setShow(Platform.OS === "ios");
+		setDate(currentDate);
+	};
+
+	const showMode = (currentMode) => {
+		setShow(true);
+		setMode(currentMode);
+	};
+
+	const showDatepicker = () => {
+		showMode("date");
+	};
+
+	const showTimepicker = () => {
+		showMode("time");
 	};
 
 	return (
-		<View style={styles.container}>
-			<Calendar onChange={this.onChange} value={this.state.date} />
+		<View>
+			<View>
+				<Button onPress={showDatepicker} title="Show date picker!" />
+			</View>
+			<View>
+				<Button onPress={showTimepicker} title="Show time picker!" />
+			</View>
+			{show && (
+				<DateTimePicker
+					testID="dateTimePicker"
+					value={date}
+					mode={mode}
+					is24Hour={true}
+					display="default"
+					locale="pt-BR"
+					onChange={onChange}
+				/>
+			)}
 		</View>
 	);
 }
